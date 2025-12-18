cf569c2072f496d68711a93e079094916aa766af
Josh Friend <jfriend@block.xyz>
2025-12-18 16:12:45 -0500

    Min IntelliJ version is 252

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ group = pluginGroup
 version = providers.environmentVariable("IJ_PLUGIN_VERSION").getOrElse("unknown")
 
 val pluginName = providers.gradleProperty("pluginName").get()
-val sinceBuildMajorVersion = "251" // corresponds to 2025.1.x versions
+val sinceBuildMajorVersion = "252" // corresponds to 2025.2.x versions
 val sinceIdeVersionForVerification = "252.23892.409" // corresponds to the 2025.2 version
 val untilIdeVersion = providers.gradleProperty("IIC.release.version").get()
 val untilBuildMajorVersion = untilIdeVersion.substringBefore('.')
@@ -45,7 +45,7 @@ dependencies {
   implementation(project(":kotlin-eventstream2:client"))
 
   intellijPlatform {
-    intellijIdeaCommunity("2025.2.1")
+    intellijIdeaCommunity("2025.2.3")
 //    androidStudio("2024.3.1.13")
 
     bundledPlugin("com.intellij.gradle")
